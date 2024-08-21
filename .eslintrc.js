c8083cebacc00039e8e0f8ec831ba338e9d2927a
Rory Fairweather-Neylan <info2rory@gmail.com>
2024-08-21 17:57:13 -0300

    feat(config): initial commit
    initial config and dependencies
    
    init

---
+++ .eslintrc.js
@@ -0,0 +1,24 @@
+module.exports = {
+  extends: ['next/core-web-vitals', 'eslint:recommended', 'prettier'],
+  globals: {
+    JSX: true,
+    Window: true
+  },
+  plugins: ['unicorn'],
+  rules: {
+    'no-unused-vars': 0,
+    '@typescript-eslint/no-unused-vars': 0,
+    'prefer-const': 'error',
+    'react-hooks/exhaustive-deps': 'error',
+    'unicorn/filename-case': [
+      'error',
+      {
+        cases: {
+          // camelCase: true,
+          // pascalCase: true,
+          kebabCase: true
+        }
+      }
+    ]
+  }
+};
---
+++ next.config.mjs
@@ -0,0 +1,14 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  reactStrictMode: true,
+  swcMinify: true,
+  images: {
+    remotePatterns: [{ protocol: 'http', hostname: 'localhost', port: '3000' }],
+    imageSizes: [16, 32, 48, 64, 96, 128, 256, 384],
+    minimumCacheTTL: 60,
+    deviceSizes: [82, 110, 140, 640, 750, 828, 1080, 1200, 1920, 2048, 3840]
+  },
+  env: {}
+};
+
+export default nextConfig;
